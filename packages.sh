79f27ed45320a3baf72e40342c842b8019c1aa5a
royalgraphx <royalgraphx0@gmail.com>
2020-02-16 01:08:15 -0600

    Scripted Run / No Further Details

--- packages.sh
+++ packages.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/com.rgx.colorpods debs
-dpkg-deb -bZgzip projects/com.rgx.cylinder12 debs
 dpkg-deb -bZgzip projects/com.rgx.lvstatic debs
 dpkg-deb -bZgzip projects/com.rgx.minecraftpods debs
 dpkg-deb -bZgzip projects/com.rgx.digital debs
@@ -8,12 +7,10 @@ dpkg-deb -bZgzip projects/com.rgx.ghostpods debs
 dpkg-deb -bZgzip projects/com.rgx.rosegold debs
 dpkg-deb -bZgzip projects/com.rgx.purplepods debs
 dpkg-deb -bZgzip projects/com.rgx.anapods debs
-dpkg-deb -bZgzip projects/com.rgx.xenhtml debs
 dpkg-deb -bZgzip projects/com.rgx.twig debs
 dpkg-deb -bZgzip projects/com.rgx.3dbundleid debs
-dpkg-deb -bZgzip projects/com.rgx.spotilife debs
 dpkg-deb -bZgzip projects/com.rgx.sbew98bundle debs
 dpkg-deb -bZgzip projects/com.rgx.propods debs
 dpkg-deb -bZgzip projects/com.rgx.sound debs
 dpkg-deb -bZgzip projects/com.rgx.lighttube debs
-dpkg-deb -bZgzip projects/com.homemadetoast.repofinder debs
+dpkg-deb -bZgzip projects/com.rgx.13bundleid debs
